02656f62f1ebac4a58324a8d2c0ef6b7603d6450
DarkForceFREEFIRE <sadeskithma7@gmail.com>
2024-08-15 19:02:38 +0530

    Add files via upload

--- Form1.bat
+++ Form1.bat
@@ -68,8 +68,6 @@ set /p password=[95m    Password: [0m
 :: Block user 1 and show dialog box
 if "%username%"=="1" if "%password%"=="1" (
 start mshta "javascript:alert('🤬 හුකන්නෙ නැතුව දීපු Login එක ගහපන්. Admin Login ගහල හැමිනෙන්නෙ නැතුව.. ');close();" & start Control forshadow.00
-
-
 )
 
 :: Flag for successful login
